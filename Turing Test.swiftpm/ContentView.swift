81f3ea6a074bc454b0295b981681813642c535dc
gabriel <gabrieldiasgonca@gmail.com>
2022-04-09 20:34:34 -0400

    teste

--- Turing Test.swiftpm/ContentView.swift
+++ Turing Test.swiftpm/ContentView.swift
@@ -13,7 +13,7 @@ struct Introducao: View {
                         .frame(width: 600)
                         .padding()
                        
-                    Text("Groups don’t actually change the way your user interface looks, they just let us work around SwiftUI’s limitation of ten child views inside a parent – that’s text views inside a form, in this insta")
+                    Text("Groups don’t actually change the way your user interface looks, they just let")
                     .padding()
                     Button("Tap Count: \(tapCount)") {
                         self.tapCount += 1
